951bf71cdf1cff737b04daa5e71f628c20b5c586
David Thai <epicdtx1998@hotmail.com>
2023-02-26 21:06:57 +1000

    [Initial] Add redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import About from "./routes/About";
-import Careers from "./routes/Careers";
-import Home from "./routes/Home";
+import About from "./pages/About";
+import Careers from "./pages/Careers";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import "./app.css";
 
 function App() {
 	return (
-		<>
+		<div>
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/careers" element={<Careers />} />
 			</Routes>
-		</>
+		</div>
 	);
 }
 
